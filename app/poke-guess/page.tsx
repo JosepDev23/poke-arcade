df3b24ba645dd587a3710566cdc0aeb7b0d08536
JosepDev23 <josepdev23@gmail.com>
2023-06-14 17:44:36 +0200

    feat: win lose logic

--- app/poke-guess/page.tsx
+++ app/poke-guess/page.tsx
@@ -19,18 +19,46 @@ export default function PokeGuess() {
     setPokemonForImg(retrievedPokemons[Math.floor(Math.random() * 4)])
   }
 
+  const handlePokemonNameClick = (pokemonName: string) => {
+    if (pokemonName === pokemonForImg?.name) console.log('win')
+    else console.log('lose')
+  }
+
   return (
     <div className={styles.floor}>
       <div className={styles.image_wrapper}>
-        <img className={styles.pokemon_image}
-          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemonForImg?.id}.png`}
-        />
+        {pokemonForImg ?
+          <img className={styles.pokemon_image}
+            src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemonForImg?.id}.png`}
+          />
+          : <></>
+        }
       </div>
       <div className={styles.names_wrapper}>
-        <button style={{ "--clr": "#39FF14" } as React.CSSProperties}><span>{pokemons[0]?.name}</span><i></i></button>
-        <button style={{ "--clr": "#FF44CC" } as React.CSSProperties}><span>{pokemons[1]?.name}</span><i></i></button>
-        <button style={{ "--clr": "#0FF0FC" } as React.CSSProperties}><span>{pokemons[2]?.name}</span><i></i></button>
-        <button style={{ "--clr": "#8A2BE2" } as React.CSSProperties}><span>{pokemons[3]?.name}</span><i></i></button>
+        <button
+          style={{ "--clr": "#39FF14" } as React.CSSProperties}
+          onClick={() => handlePokemonNameClick(pokemons[0]?.name)}
+        >
+          <span>{pokemons[0]?.name}</span><i></i>
+        </button>
+        <button
+          style={{ "--clr": "#FF44CC" } as React.CSSProperties}
+          onClick={() => handlePokemonNameClick(pokemons[1]?.name)}
+        >
+          <span>{pokemons[1]?.name}</span><i></i>
+        </button>
+        <button
+          style={{ "--clr": "#0FF0FC" } as React.CSSProperties}
+          onClick={() => handlePokemonNameClick(pokemons[2]?.name)}
+        >
+          <span>{pokemons[2]?.name}</span><i></i>
+        </button>
+        <button
+          style={{ "--clr": "#8A2BE2" } as React.CSSProperties}
+          onClick={() => handlePokemonNameClick(pokemons[3]?.name)}
+        >
+          <span>{pokemons[3]?.name}</span><i></i>
+        </button>
       </div>
     </div>
   )
