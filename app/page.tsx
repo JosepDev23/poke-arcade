72105b8de7b509b9d9d81492e6f06ed2284abdf7
JosepDev23 <josepdev23@gmail.com>
2024-04-24 22:14:42 +0200

    feat: new cards

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,22 @@
-'use client'
-import { useRouter } from 'next/navigation'
-import styles from './home.module.css'
+"use client";
+import { useRouter } from "next/navigation";
+import styles from "./home.module.css";
 
 export default function Home() {
-
-  const router = useRouter()
+  const router = useRouter();
 
   const handleClickPokeGuess = () => {
-    router.push('/poke-guess')
-  }
+    router.push("/poke-guess");
+  };
 
   return (
     <div className={styles.floor}>
       <div className={styles.cards_container}>
         <article onClick={handleClickPokeGuess} className={styles.card_wrapper}>
-          <img src='/pokeguess_background.png' />
-          <img src='/pokeguess_over.png' />
-        </article>
-        <article onClick={handleClickPokeGuess} className={styles.card_wrapper}>
-          <img src='/pokeguess_background.png' />
-          <img src='/pokeguess_over.png' />
-        </article>
-        <article onClick={handleClickPokeGuess} className={styles.card_wrapper}>
-          <img src='/pokeguess_background.png' />
-          <img src='/pokeguess_over.png' />
+          <img src="/pokeguess_background.jpg" />
+          <img src="/pokeguess_over.png" />
         </article>
       </div>
     </div>
-  )
+  );
 }
