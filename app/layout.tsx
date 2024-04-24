680a83018312c06a880871df790f4a8203e860fd
JosepDev23 <josepdev23@gmail.com>
2024-04-24 21:51:20 +0200

    remove ugly and old nav bar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,31 +1,26 @@
-import './globals.css'
-import styles from './layout.module.css'
-import React from 'react'
-import Providers from './utils/providers'
+import "./globals.css";
+import styles from "./layout.module.css";
+import React from "react";
+import Providers from "./utils/providers";
 
 export const metadata = {
-  title: 'PokeArcade',
-  description: 'Pokemon web app by JosepDev23',
-}
+  title: "PokeArcade",
+  description: "Pokemon web app by JosepDev23",
+};
 
 export default async function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
-
   return (
     <html lang="en">
       <body>
-        <div className={styles.nav_bar}>
-          <img className={styles.logo} src='/pokearcade_logo.jpeg' />
-        </div>
+        <img className={styles.logo} src="/pokearcade_logo.jpeg" />
         <div className={styles.body}>
-          <Providers>
-            {children}
-          </Providers>
+          <Providers>{children}</Providers>
         </div>
       </body>
     </html>
-  )
+  );
 }
